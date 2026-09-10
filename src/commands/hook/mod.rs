822c3d5920f4d2135ee66533fc1dc4a4f315206b
Jon-Luke Biddle <jonluke.biddle@gmail.com>
2026-09-09 22:54:37 -0600

    lifetime lint

--- src/commands/hook/mod.rs
+++ src/commands/hook/mod.rs
@@ -867,14 +867,14 @@ fn env_parse<T: FromStr>(key: &str) -> Option<T> {
 /// session. The enabled variant uses a non-panicking write so a broken stderr
 /// pipe can't break the session.
 #[cfg(feature = "hook-debug")]
-fn debug(args: std::fmt::Arguments) {
+fn debug(args: std::fmt::Arguments<'_>) {
     use std::io::Write;
     let _ = writeln!(std::io::stderr().lock(), "semctl hook: {args}");
 }
 
 #[cfg(not(feature = "hook-debug"))]
 #[inline(always)]
-fn debug(_args: std::fmt::Arguments) {}
+fn debug(_args: std::fmt::Arguments<'_>) {}
 
 #[cfg(test)]
 mod tests;
